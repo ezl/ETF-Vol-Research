dec1d4d53ee5c4309a7cba915b43607080b2022c
E <E@E.(none)>
2010-03-15 02:33:06 -0500

    add graphs to visually check product relationships

--- SQL_inverse_product_correlation.py
+++ SQL_inverse_product_correlation.py
@@ -1,8 +1,25 @@
-get_fit_parameter = """
-            SELECT symbol1.%(fit_parameter)s AS %(fit_parameter)s1, symbol2.%(fit_parameter)s AS %(fit_parameter)s2
-            FROM (SELECT * FROM VolSmile WHERE symbol='%(symbol1)s') symbol1
-            JOIN (SELECT * FROM VolSmile WHERE symbol='%(symbol2)s') symbol2
+get_fit_coeff = """
+            SELECT
+                symbol1.%(fit_parameter)s AS coeff1,
+                symbol2.%(fit_parameter)s AS coeff2,
+                symbol1.t_date AS t_date,
+                symbol1.price_close AS price_close1,
+                symbol2.price_close AS price_close2
+            FROM
+                    (SELECT *
+                    FROM (SELECT * FROM VolSmile WHERE symbol='%(symbol1)s') vs
+                    JOIN StockPrice
+                        ON StockPrice.symbol=vs.symbol
+                        AND StockPrice.t_date=vs.t_date
+                    ) symbol1
+            JOIN
+                    (SELECT *
+                    FROM (SELECT * FROM VolSmile WHERE symbol='%(symbol2)s') vs
+                    JOIN StockPrice
+                        ON StockPrice.symbol=vs.symbol
+                        AND StockPrice.t_date=vs.t_date
+                    ) symbol2
             ON symbol1.t_date=symbol2.t_date
             AND symbol1.expiration_date=symbol2.expiration_date
-            WHERE symbol1.t_date > %(start_date)s
-            AND symbol1.t_date < %(end_date)s"""
+            WHERE symbol1.t_date > '%(start_date)s'
+            AND symbol1.t_date < '%(end_date)s'"""
--- inverse_product_correlation.py
+++ inverse_product_correlation.py
@@ -18,7 +18,7 @@ conn = sqlite3.connect("Levered_Financial_ETF_Option_and_Stock_DB.sqlite3")
 pair = ('FAS','FAZ')
 symbol1 = 'FAS'
 symbol2 = 'FAZ'
-fit_parameter = 'vol'
+fit_parameter = 'skew'
 start_date = datetime.date(2009, 1, 2)
 end_date = datetime.date(2009, 11, 12)
 
@@ -28,7 +28,48 @@ params['symbol2'] = symbol2
 params['fit_parameter'] = fit_parameter
 params['start_date'] = int(datetime2epoch(start_date))
 params['end_date'] = int(datetime2epoch(end_date))
-sql = SQL.get_fit_parameter % params
+sql = SQL.get_fit_coeff % params
 print sql
-fit_parameter = execute_query_DF(conn, sql)
+fit_coeff = execute_query_DF(conn, sql)
+coeff1 = fit_coeff('coeff1')
+coeff2 = fit_coeff('coeff2')
+price1 = fit_coeff('price_close1')
+price2 = fit_coeff('price_close2')
+t_dates = fit_coeff('t_date')
+
+# Linear fit
+
+linear_fit = scipy.polyfit(coeff1, coeff2, deg=1)
+xmin = min(coeff1)
+xmax = max(coeff1)
+fit_x = np.arange(xmin, xmax, (xmax - xmin) / 50.)
+fit_y = scipy.polyval(linear_fit, fit_x)
+
+# Let's make some pretty pictures!
+pyplot.subplot(2,3,1)
+pyplot.plot(coeff1, coeff2, '.')
+pyplot.plot(fit_x, fit_y, 'r-')
+pyplot.xlabel(symbol1)
+pyplot.ylabel(symbol2)
+pyplot.title("%s, abs" % fit_parameter)
+
+pyplot.subplot(3,3,2)
+pyplot.plot(np.log(price1/price2), np.log(coeff1/coeff2), '.')
+pyplot.title("%s, log(c1/c2) v log(px1/px2)" % fit_parameter)
+
+pyplot.subplot(2,3,3)
+pyplot.plot(t_dates, coeff1-coeff2, '.')
+
+pyplot.twinx()
+pyplot.plot(t_dates, price1, "r-")
+
+
+pyplot.subplot(2,3,4)
+pyplot.plot(t_dates, price1)
+pyplot.title(symbol1)
+
+pyplot.subplot(2,3,5)
+pyplot.plot(t_dates, price2)
+pyplot.title(symbol2)
+pyplot.show()
 ipshell("")
