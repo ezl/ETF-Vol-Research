593141ed397f2313f203637d8972ab460964361e
e <rock357@Rock357.(none)>
2010-03-19 16:57:47 -0500

    animate vol smiles

--- view_smiles.py
+++ view_smiles.py
@@ -14,10 +14,17 @@ import SQL_view_smiles as SQL
 import scipy
 import datetime
 from matplotlib import pyplot
+from time import sleep
 
 conn = sqlite3.connect("Levered_Financial_ETF_Option_and_Stock_DB.sqlite3")
 
-symbols = ['FAS','FAZ','SKF','UYG']
+symbols = ['FAS',]
+
+pyplot.ion()
+calls, puts, wt_averages, best_fit = pyplot.plot(3, 3, 'ro',
+                                                 3, 3, 'bo',
+                                                 3, 3, 'k*',
+                                                 3, 3, 'k-')
 
 for symbol in symbols:
     params = dict()
@@ -35,8 +42,6 @@ for symbol in symbols:
             sql = SQL.get_implied_forward % params
             implied_forward = execute_query_DF(conn, sql)('implied_forward')[0]
             implied_vols = {}; strikes = {}
-            vol = {}; skew = {}; kurt = {}; wave = {}; wing = {}
-            z = {}; fit_vols = {}
             sql = SQL.get_strike_and_cvol_and_pvol_and_call_delta % params
             vols = execute_query_DF(conn, sql)
             call_ivs = vols('call_iv')
@@ -63,36 +68,39 @@ for symbol in symbols:
             coeffs = [wing, wave, kurt, skew, vol]
             fit_vols = scipy.polyval(coeffs, z)
 
-            # set colors for calls/puts
-            pyplot.plot(strikes, call_ivs, 'ro')
-            pyplot.plot(strikes, put_ivs, 'bo')
-            pyplot.plot(strikes, wt_avg_implied_vols, 'k*')
-            pyplot.plot(strikes, fit_vols, 'k-')
-            print "IF: %s".rjust(20) % implied_forward, "||",
-            for prime in [vol, skew, kurt, wave, wing]:
-                print prime,
-            print
-            xmin = min(strikes)
-            xmax = max(strikes)
+#            xmin = min(strikes)
+#            xmax = max(strikes)
+            xmin = int(implied_forward * 1/2)
+            xmax = int(implied_forward * 3/2)
             ymin = min(np.hstack((call_ivs, put_ivs)))
             ymax = max(np.hstack((call_ivs, put_ivs)))
             pyplot.axvline(x=implied_forward, ymin=0, ymax=1, linewidth=10, color="#B0E0E6", zorder=0)
-
-#             pyplot.axis([xmin, xmax, ymin, ymax])
-            pyplot.title("date: %s, exp: %s, points:%s" % (
+            pyplot.axis([xmin, xmax, ymin, ymax])
+            pyplot.title("F: %s, date: %s, exp: %s, points:%s" % (
+                                                            implied_forward,
                                                             str(epoch2datetime(int(t_date)).date()),
                                                             str(epoch2datetime(int(expiration_date)).date()),
                                                             len(strikes) )
                         )
-
-            primes = """
-            vol : %s
-            skew: %s
-            kurt: %s
-            wave: %s
-            wing: %s""" % (vol, skew, kurt, wave,wing)
-            pyplot.text(xmin, ymin, primes)
-            pyplot.show()
-            # raise Exception
-            exit()
+            graphs = [calls, puts, wt_averages, best_fit]
+            y_data = [call_ivs, put_ivs, wt_avg_implied_vols, fit_vols]
+            calls.set_xdata(strikes)
+            time.sleep(.5)
+            for graph, y in zip(graphs, y_data):
+                graph.set_xdata(strikes)
+                graph.set_ydata(y)
+                pyplot.draw()
+            print "IF: %s".rjust(20) % implied_forward, "||",
+            for prime in [vol, skew, kurt, wave, wing]:
+                print prime,
+            print
+#
+#            primes = """
+#            vol : %s
+#            skew: %s
+#            kurt: %s
+#            wave: %s
+#            wing: %s""" % (vol, skew, kurt, wave,wing)
+#            pyplot.text(xmin, ymin, primes)
+#            pyplot.show()
 
