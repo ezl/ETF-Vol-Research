cc3ddd73313eb1200a9e4b1d0a8f3005cc0e4f57
e <rock357@Rock357.(none)>
2010-03-19 17:06:44 -0500

    animate alpha curves

--- fit_alpha_curves.py
+++ fit_alpha_curves.py
@@ -12,6 +12,7 @@ import sqlite3tools
 import SQL_fit_alpha_curves as SQL
 from datetimetools import * 
 from ipshell import ipshell
+from time import sleep
 
 TABLENAME = "AlphaCurve"
 DATABASENAME = "Levered_Financial_ETF_Option_and_Stock_DB.sqlite3"
@@ -34,6 +35,11 @@ debug_symbols = ['FAS']
 # will report errors (multiple hits for unique data)
 if not DEBUG is True:
     sqlite3tools.delete_sqlite_table(conn, TABLENAME)
+
+if DEBUG is True:
+    pyplot.ion()
+    alpha_curve, = pyplot.plot(0,0, 'bo')
+    pyplot.axis([0,500,.2,3.5])
 
 if DEBUG is True and debug_symbols is not None:
     symbols = debug_symbols
@@ -58,14 +64,13 @@ for symbol in symbols:
         def alpha(stv, mv, alpha):
             pivot = 100
             vol = mv + (stv - mv) * () * (stt/t) * np.sqrt(stt/(pivot/365))
-        
+
         if DEBUG is True:
-            pyplot.plot(days, vol, 'bo')
-            pyplot.axis([0,500,.2,3.5])
+            sleep(.1)
             pyplot.title(epoch2datetime(int(t_date)).date())
-
-
-            pyplot.show()
+            alpha_curve.set_xdata(days)
+            alpha_curve.set_ydata(vol)
+            pyplot.draw()
 
         if DEBUG is False:
             pass
@@ -76,5 +81,3 @@ for symbol in symbols:
 #                       implied_forward, implied_rate,
 #                       coeffs[4], coeffs[3], coeffs[2], coeffs[1], coeffs[0])
 #            sqlite3tools.insert_to_sqlite(conn, TABLENAME, columns, results)
-        exit()
-        pyplot.cla()
