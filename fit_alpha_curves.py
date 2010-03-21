1aeec7055b685556fd8a74cf29b29081ae4707d8
e <rock357@Rock357.(none)>
2010-03-21 17:20:01 -0500

    fit, plot, and insert to DB the alpha of best fit

--- fit_alpha_curves.py
+++ fit_alpha_curves.py
@@ -24,7 +24,8 @@ skipped = 0
 fail = 0
 smile_count = 0
 
-DEBUG = True
+DEBUG = False
+GRAPH = True
 # For all expiration_dates, set debug_expiration_dates = None
 debug_t_dates = [1226901600,]
 debug_t_dates = None
@@ -35,10 +36,17 @@ debug_symbols = ['FAS']
 if not DEBUG is True:
     sqlite3tools.delete_sqlite_table(conn, TABLENAME)
 
-if DEBUG is True:
+if GRAPH is True:
     pyplot.ion()
-    alpha_curve, = pyplot.plot(0,0, 'bo')
-    pyplot.axis([0,500,.2,3.5])
+    term_vols, alpha_curve = pyplot.plot(0, 0, 'bo',
+                                         0, 0, 'k-')
+    xmin = 0
+    xmax = 500
+    ymin = .2
+    ymax = 3.5
+    pyplot.axis([xmin, xmax, ymin, ymax])
+
+    alpha_days = np.arange(0, 500, 2)
 
 if DEBUG is True and debug_symbols is not None:
     symbols = debug_symbols
@@ -58,25 +66,27 @@ for symbol in symbols:
             continue
         expiration_dates = np.array([int(i) for i in data('expiration_date')])
         days = (expiration_dates - int(t_date)) / seconds_per_day
+        times_to_expiration = days / 365.
 
         vol = data('vol')
-        def alpha(stv, mv, alpha):
-            pivot = 100
-            vol = mv + (stv - mv) * () * (stt/t) * np.sqrt(stt/(pivot/365))
 
-        if DEBUG is True:
+        from alpha import alpha_theo_vol
+        coeffs = nerdy.wls_fit(alpha_theo_vol,[1,.7,5], times_to_expiration, vol, lb=[.3,.2,1],ub=[5,3,10])
+        alpha_vols = alpha_theo_vol(coeffs, alpha_days/365.)
+
+        if GRAPH is True:
             sleep(.1)
             pyplot.title(epoch2datetime(int(t_date)).date())
-            alpha_curve.set_xdata(days)
-            alpha_curve.set_ydata(vol)
+            term_vols.set_xdata(days)
+            term_vols.set_ydata(vol)
+            alpha_curve.set_xdata(alpha_days)
+            alpha_curve.set_ydata(alpha_vols)
             pyplot.draw()
 
         if DEBUG is False:
             pass
-#            columns = ('symbol', 't_date', 'expiration_date',
-#                       'implied_forward', 'implied_rate',
-#                       'vol', 'skew', 'kurt', 'wave', 'wing')
-#            results = (symbol, t_date, expiration_date,
-#                       implied_forward, implied_rate,
-#                       coeffs[4], coeffs[3], coeffs[2], coeffs[1], coeffs[0])
-#            sqlite3tools.insert_to_sqlite(conn, TABLENAME, columns, results)
+            columns = ('symbol', 't_date',
+                       'pivot_vol', 'mean_vol', 'alpha', 'expirations')
+            results = (symbol, t_date,
+                       coeffs[0], coeffs[1], coeffs[2], len(expiration_dates))
+            sqlite3tools.insert_to_sqlite(conn, TABLENAME, columns, results)
