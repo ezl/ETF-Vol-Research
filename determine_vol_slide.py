e7af8a9b602c34e368213f05af67835ef18088b3
e <rock357@Rock357.(none)>
2010-03-21 20:15:31 -0500

    set date beginning date for st_vol/px regression
    suspicion that early fits were bad or data was bad.
    not really the case, still no good pivot_vol regression

--- determine_vol_slide.py
+++ determine_vol_slide.py
@@ -1,3 +1,5 @@
+from datetime import date
+from datetimetools import *
 from ipshell import ipshell
 from matplotlib import pyplot
 import numpy as np
@@ -8,12 +10,15 @@ import sqlite3
 DBNAME = "Levered_Financial_ETF_Option_and_Stock_DB.sqlite3"
 conn = sqlite3.connect(DBNAME)
 
+start_date = date(2009, 6, 1)
+
 SQL = """SELECT AlphaCurve.*, StockPrice.price_close AS spot, StockPrice.t_date AS stock_date
          FROM AlphaCurve JOIN StockPrice
          ON AlphaCurve.t_date=StockPrice.t_date
          AND AlphaCurve.symbol=StockPrice.symbol
          WHERE AlphaCurve.symbol='FAS'
-"""
+         AND AlphaCurve.t_date > %s
+""" % datetime2epoch(start_date)
 
 results = execute_query_DF(conn, SQL)
 
